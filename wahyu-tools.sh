207d048202d7eeb119c71c15fae259540de219e9
KINxY <76677018+brgin@users.noreply.github.com>
2021-12-06 20:08:51 +0700

    Update wahyu-tools.sh

--- wahyu-tools.sh
+++ wahyu-tools.sh
@@ -1,31 +1,52 @@
-clear
-blue='\033[34;1m'                                                     green='\033[32;1m'                                                    purple='\033[35;1m'
-cyan='\033[36;1m'                                                     red='\033[31;1m'                                                      white='\033[37;1m'
-yellow='\033[33;1m'                                                   sleep 1
+clear                                                                 blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
 echo
-echo $green"WELKOM TO WAHYU TOOLS"                                    sleep 2                                                               echo $green"Jangan Lupa Subscribe Sunshine Juhari di Youtube Yahaha W>
-echo                                                                  sleep 2                                                               echo $yellow"Pilih Tools Nya Ya Tod :"                                echo                                                                  echo $cyan"1.) STABILKAN JARINGAN WAHYU"
+echo $green"WELKOM TO WAHYU TOOLS"
+sleep 2
+echo $green"Jangan Lupa Subscribe Sunshine Juhari di Youtube Yahaha Wa
+hyu"
+sleep 2
+echo $yellow"Pilih Tools Nya Ya Tod :"
 echo
-echo $blue"0.) Keluar Sebagai WahyuRoar"
+echo $cyan"1.) STABILKAN JARINGAN WAHYU"
+echo
+echo $blue"2.) Keluar Sebagai WahyuRoar"
+echo
+echo $green"3.) Phising Berjenis Pulsa Gratis"
 echo
 echo $white
-read -p "Pilihmana Tod : " bro                                        
-if [ $bro = 1 ] || [ $bro = 1 ]
-then
-clear
+read -p "Pilihmana Tod : " bro                                                                                                              if [ $bro = 1 ] || [ $bro = 1 ]
+then                                                                  clear
 echo $green"STABILKAN JARINGAN WAHYU"
 sleep 2
 ping -s1000 1.1.1.1
 fi
 
-if [ $bro = 0 ] || [ $bro = 0 ]
+if [ $bro = 2 ] || [ $bro = 2 ]
 then
 clear
 echo $cyan
 figlet "BYE"
 echo $white "To Be Continued Ya Tod"
 sleep 2
-echo $green "Semoga hari mu menyenangkan Ya Pren aowkkwkw kagak ada emote nya yahaha wahyu roar"
+echo $green "Semoga hari mu menyenangkan Ya Pren baik ku"
 sleep 3
 exit
-if
+fi
+
+if [ $bro = 3] || [ $bro = 3 ]
+then
+echo $white "https://grabify.link/M3LEVV ← Kirimkan Link Ini"
+sleep 2
+echo $green "Dan Chat aku"
+echo $green "nomor : 0895332355556"
+sleep 2
+echo $white "Maka Aku akan melacak alamat tersebut"
+exit
+fi
