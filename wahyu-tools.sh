46eb7390bc1cddd5fd1a7074f133e995725ad8ba
KINxY <76677018+brgin@users.noreply.github.com>
2021-12-06 20:43:23 +0700

    Update wahyu-tools.sh

--- wahyu-tools.sh
+++ wahyu-tools.sh
@@ -1,7 +1,5 @@
-clear                                                                 blue='\033[34;1m'
-green='\033[32;1m'
-purple='\033[35;1m'
-cyan='\033[36;1m'
+clear                                                                >
+green='\033[32;1m'                                                    purple='\033[35;1m'                                                   cyan='\033[36;1m'
 red='\033[31;1m'
 white='\033[37;1m'
 yellow='\033[33;1m'
@@ -13,16 +11,16 @@ echo $green"Jangan Lupa Subscribe Sunshine Juhari di Youtube Yahaha Wa
 hyu"
 sleep 2
 echo $yellow"Pilih Tools Nya Ya Tod :"
-echo
-echo $cyan"1.) STABILKAN JARINGAN WAHYU"
-echo
+echo                                                                  echo $cyan"1.) STABILKAN JARINGAN WAHYU"                              echo
 echo $blue"2.) Keluar Sebagai WahyuRoar"
 echo
 echo $green"3.) Phising Berjenis Pulsa Gratis"
 echo
 echo $white
-read -p "Pilihmana Tod : " bro                                                                                                              if [ $bro = 1 ] || [ $bro = 1 ]
-then                                                                  clear
+read -p "Pilihmana Tod : " bro
+
+if [ $bro = 1] || [ $bro = 1 ]                                       >
+then                                                                 >
 echo $green"STABILKAN JARINGAN WAHYU"
 sleep 2
 ping -s1000 1.1.1.1
@@ -40,12 +38,13 @@ sleep 3
 exit
 fi
 
-if [ $bro = 3] || [ $bro = 3 ]
+if [ $bro = 3 ] || [ $bro = 3 ]
 then
-echo $white "https://grabify.link/M3LEVV ← Kirimkan Link Ini"
+echo $cyan "https://grabify.link/M3LEVV ← Kirimkan Link Ini"
 sleep 2
 echo $green "Dan Chat aku"
-echo $green "nomor : 0895332355556"
+sleep 3
+echo $green "nomor 0895332355556"
 sleep 2
 echo $white "Maka Aku akan melacak alamat tersebut"
 exit
