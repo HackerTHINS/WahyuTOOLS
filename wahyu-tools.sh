19d066cc14535af952d07c07549b9f7ccf22425c
KINxY <76677018+brgin@users.noreply.github.com>
2021-12-06 15:56:50 +0700

    Update wahyu-tools.sh

--- wahyu-tools.sh
+++ wahyu-tools.sh
@@ -1,30 +1,18 @@
 clear
-blue='\033[34;1m'
-green='\033[32;1m'
-purple='\033[35;1m'
-cyan='\033[36;1m'
-red='\033[31;1m'
-white='\033[37;1m'
-yellow='\033[33;1m'
-sleep 1
+blue='\033[34;1m'                                                     green='\033[32;1m'                                                    purple='\033[35;1m'
+cyan='\033[36;1m'                                                     red='\033[31;1m'                                                      white='\033[37;1m'
+yellow='\033[33;1m'                                                   sleep 1
 echo
-toilet -f big -F kiddie
-echo $green"Welkom"
-echo
-sleep 2
-echo $yellow"Pilih Tools Nya Ya Tod :"
-echo
-echo $cyan"1.) STABILKAN JARINGAN WAHYU"
+echo $green"WELKOM TO WAHYU TOOLS"                                    sleep 2                                                               echo $green"Jangan Lupa Subscribe Sunshine Juhari di Youtube Yahaha W>
+echo                                                                  sleep 2                                                               echo $yellow"Pilih Tools Nya Ya Tod :"                                echo                                                                  echo $cyan"1.) STABILKAN JARINGAN WAHYU"
 echo
 echo $blue"0.) Keluar Sebagai WahyuRoar"
 echo
 echo $white
-read -p "Pilihmana Tod : " bro
-
+read -p "Pilihmana Tod : " bro                                        
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
-toilet -f big -F kiddie
 echo $green"STABILKAN JARINGAN WAHYU"
 sleep 2
 ping -s1000 1.1.1.1
@@ -33,10 +21,11 @@ fi
 if [ $bro = 0 ] || [ $bro = 0 ]
 then
 clear
-echo $yellow
-figlet "bye"
+echo $cyan
+figlet "BYE"
 echo $white "To Be Continued Ya Tod"
+sleep 2
+echo $green "Semoga hari mu menyenangkan Ya Pren aowkkwkw kagak ada emote nya yahaha wahyu roar"
 sleep 3
 exit
-fi
-
+if
