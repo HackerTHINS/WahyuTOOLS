0ccbb0284034e63882b8e97cde42fff3c79c61c2
KINxY <76677018+brgin@users.noreply.github.com>
2021-12-07 15:01:35 +0700

    Add files via upload

---
+++ wahyu-tools.sh
@@ -0,0 +1,86 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay kiddie
+echo $green"Welcome To Wahyu Tools Tuds"
+echo
+echo $green"SEMOGA HARI MU SURAM YAHAHA BERCANDA"
+echo
+sleep 2
+echo $yellow"Pilih Tools Nya Ya TUDS:"
+echo
+echo $cyan"1.) STABILKAN JARINGAN WAHYU MEN"
+echo
+echo $blue"2.) Keluar Sebagai WahyuRoar"
+echo
+echo $green"3.) Link Pelacak Ip"
+echo
+echo $red"4.) Update Tools"
+echo
+echo $white
+read -p "Pilihmana Tod : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Kiddie
+echo $green"STABILKAN JARINGAN WAHYU"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $yellow
+figlet "bye"
+echo $white "To Be Continued Ya Wahyu Roar"
+toilet -f big -F gay Roarrr
+sleep 3
+exit
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f big -F gay Link
+echo $green  "https://2no.co/KIXH nih link nya"
+sleep 2
+echo $green "nomor aku 0895332355556"
+sleep 2
+echo $green "nanti aku bakalan lacak alamat nya"
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+toilet -f big -F gay update...
+sleep 3
+echo $green "no update detected"
+sleep 2
+echo $white"System Error"
+sleep 2
+toilet -f big -F gay System
+
+echo $green "Force Back"
+sleep 4
+toilet -f big -F gay Error
+sleep 2
+echo $green "Tidak Bisa Kembali, Sistem memaksa untuk keluar"
+sleep 2
+toilet -f big -F gay EXIT...
+sleep 2
+echo $yellow "Force Exit"
+sleep 2
+figlet "Force..."
+sleep 2
+exit
+fi
+
+
