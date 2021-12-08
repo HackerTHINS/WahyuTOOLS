2fa27dac902a39700a0517499ae6e483aa1dcca3
KINxY <76677018+brgin@users.noreply.github.com>
2021-12-08 17:18:33 +0700

    Add files via upload

---
+++ install-bahan.sh
@@ -0,0 +1,29 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Install...
+sleep 3
+echo $red "Ready To Install"
+sleep 2
+figlet "Loading..."
+pkg install mc
+pkg install figlet
+pkg install toilet
+pkg install python
+pkg update
+sleep 2
+echo $green "WELKOM"
+figlet "Loading..." 
+sleep 2
+toilet -f big -F gay Exit
+exit
+fi
+
+
