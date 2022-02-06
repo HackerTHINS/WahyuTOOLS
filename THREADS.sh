de7273f8ed2bacd695ed0e6b024bff1337293ec6
KINxY <76677018+brgin@users.noreply.github.com>
2022-02-06 10:30:13 +0700

    Add files via upload

---
+++ THREADS.sh
@@ -0,0 +1,54 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay GARRYMOD
+sleep 2
+echo $green "INSTALL..."
+pkg install mc
+pkg install python2
+sleep 2
+echo $green "INSTALLING NO ROOT SYS"
+pkg install python3
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+pkg install git -y
+pkg install mc
+pkg install python2 -y
+toilet -f small -F gay REQUIRED A Permissions
+termux-setup-storage
+echo $white "Permission located to /sdcard/android/system/root"
+sleep 2
+echo $green "TRANSFER THREADS TO LOCATION AREA....."
+sleep 1
+echo $white "PLS TURN ON YOUR LOCATION !"
+sleep 8
+echo $white "TARGET TO 192.168.1.1"
+sleep 1
+echo $white "Running SystemROOT"
+echo
+read -p "NAME OF TARGET : " ABC
+sleep 1
+echo $white "SEND 230rFc.api.com"
+sleep 1
+echo $red "Please turn on show pop-up window, this permissions required"
+sleep 7
+pkg update
+pkg upgrade
+echo $red "STARTING TARGET"
+sleep 1
+echo $red "SEND ALL THEARDS"
+sleep 1
+echo $red "COPYING IP ADDRES"
+sleep 1
+echo $red "BREAKING IPB-12"
+sleep 1
+cowsay BREAK SUCCEED
+exit
+fi
+
