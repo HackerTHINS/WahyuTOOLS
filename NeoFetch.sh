2e363d7e404efb725ba5633343ee94a302f71391
KINxY <76677018+brgin@users.noreply.github.com>
2022-02-09 19:35:39 +0700

    Create NeoFetch.sh

---
+++ NeoFetch.sh
@@ -0,0 +1,24 @@
+clear
+sleep 1
+echo "FOR VSHELL!!!"
+sleep 10
+echo "vShell Starting"
+sleep 1
+echo "installing neofetch"
+sleep 2
+echo "Update..."
+apk upgrade
+sleep 2
+apk update
+sleep 2
+echo "install neofetch"
+apk add neofetch
+neofetch
+sleep 1
+echo "Type root to login"
+login
+sleep 1
+apk --help
+neofetch --help
+exit
+fi
